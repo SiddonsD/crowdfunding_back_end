d2d29f528a682854cb16b895e5456d7d95ee0fcc
SiddonsD <denise.siddons@gmail.com>
2024-01-28 21:09:43 +0800

    user url debug test

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns =[
     path('users/', views.CustomUserList.as_view()),
     path('users/register', views.CustomUserRegister.as_view()),
     path('user/<int:pk>/', views.CustomUserDetail.as_view()),
-    path('user/change_password/', views.ChangePasswordView.as_view())
+    path('user/change_password_test/', views.ChangePasswordView.as_view())
 ]
