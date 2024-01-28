3e47f7c98da140003913d6a37690e7b694f0043b
SiddonsD <denise.siddons@gmail.com>
2024-01-28 19:35:37 +0800

    update urls

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns =[
     path('users/', views.CustomUserList.as_view()),
     path('users/register', views.CustomUserRegister.as_view()),
     path('user/<int:pk>/', views.CustomUserDetail.as_view()),
-    path('/change_password/', views.ChangePasswordView.as_view()),
+    path('user/<int:pk>/change_password/', views.ChangePasswordView.as_view()),
 ]
