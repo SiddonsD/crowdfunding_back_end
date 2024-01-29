ad8df45e83f0556027b4d0d1d9de8f9adfd54217
SiddonsD <denise.siddons@gmail.com>
2024-01-29 10:35:22 +0800

    push profileupdate to remote

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -17,3 +17,36 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+# user update profile feature modified from https://medium.com/django-rest/django-rest-framework-change-password-and-update-profile-1db0c144c0a3
+    
+class UpdateUserSeralizer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        Model = CustomUser
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {'password': {'read_only': True}}
+
+    def validate_email (self, value):
+        user = self.context['request'].user
+        if CustomUser.objects.exclude(pk=user.pk).filter(email=value).exists():
+            raise serializers.ValidationError({"email": "This email is already exists."})
+        return value
+    
+    def validate_username(self, value):
+        user = self.context['request'].user
+        if CustomUser.objects.exclude(pk=user.pk).filter(username=value).exists():
+            raise serializers.ValidationError({"username": "This username is already in use."})
+        return value
+    
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.email = validated_data['email']
+        instance.username = validated_data['username']
+
+        instance.save()
+
+        return instance 
+
