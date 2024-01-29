d55d4b5d440867506bead9e91af739508201d6a4
SiddonsD <denise.siddons@gmail.com>
2024-01-29 11:06:04 +0800

    urls update

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns =[
     path('users/', views.CustomUserList.as_view()),
-    path('users/register', views.CustomUserRegister.as_view()),
+    path('users/register/', views.CustomUserRegister.as_view()),
     path('user/<int:pk>/', views.CustomUserDetail.as_view()),
-    path('user/update_profile/<int:pk>/', views.UpdateProfileView.as_view())
+    path('user/update_profile/<int:pk>/', views.UpdateProfileView.as_view()),
 ]
