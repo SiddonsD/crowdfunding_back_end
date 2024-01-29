9c9c06307719f157e56f689d45e8612d0b650826
Denise Siddons <146698245+SiddonsD@users.noreply.github.com>
2024-01-29 14:45:35 +0800

    Update urls.py

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns =[
     path('users/', views.CustomUserList.as_view()),
     path('users/register', views.CustomUserRegister.as_view()),
-    path('user/<int:pk>/', views.CustomUserDetail.as_view()),
+    path('user-test/<int:pk>/', views.CustomUserDetail.as_view()),
     path('user/change_password/<int:pk>/', views.ChangePasswordView.as_view())
 ]
