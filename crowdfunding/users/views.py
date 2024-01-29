0613a54a1f08495433bc7c379e93f3eafab90202
SiddonsD <denise.siddons@gmail.com>
2024-01-29 16:30:41 +0800

    update user views

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -2,6 +2,7 @@ from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 from django.http import Http404
 from rest_framework.views import APIView
+from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer, ChangePasswordSerializer, UpdateProfileSeralizer
@@ -46,8 +47,8 @@ class ChangePasswordView(generics.UpdateAPIView):
         permission_classes = (IsAuthenticated,)
         serializer_class = ChangePasswordSerializer
 
-class UpdateProfileView (generics, APIView):
+class UpdateProfileView (UpdateAPIView):
 
     queryset = CustomUser.objects.all()
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated)
     serializer_class = UpdateProfileSeralizer
