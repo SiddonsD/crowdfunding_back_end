94338e7241385f68c5b2bc0bfdda6b8e2bef11aa
SiddonsD <denise.siddons@gmail.com>
2024-03-06 21:08:44 +0800

    adds custom auth token class to users views.py

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from django.http import Http404
 from rest_framework.views import APIView
@@ -23,6 +24,21 @@ class CustomUserRegister (APIView):
             return Response(response_data, status=status.HTTP_201_CREATED)
         
         return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
 
 class CustomUserList (APIView):
 
