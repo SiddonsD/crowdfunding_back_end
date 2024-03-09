21f367fb99cd2440aa82d346d69ad22d389f334e
SiddonsD <denise.siddons@gmail.com>
2024-03-09 15:52:22 +0800

    updates users urls.py with api token path

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -6,5 +6,6 @@ urlpatterns =[
     path('users/register', views.CustomUserRegister.as_view()),
     path('user/<int:pk>/', views.CustomUserDetail.as_view()),
     path('user/change_password/<int:pk>/', views.ChangePasswordView.as_view()),
-    path('user/update_profile/<int:pk>/', views.UpdateProfileView.as_view())
+    path('user/update_profile/<int:pk>/', views.UpdateProfileView.as_view()),
+    path('api-token-auth/', views.CustomAuthToken.as_view(), name='api_token_auth')
 ]
