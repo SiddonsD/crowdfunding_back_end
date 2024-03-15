edbf386a1d154f1931f16cd793fcfbff726166d3
SiddonsD <denise.siddons@gmail.com>
2024-03-15 15:44:11 +0800

    configs project urls

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
-    path('projects/create', views.ProjectCreate.as_view()),
-    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
+    path('project/create', views.ProjectCreate.as_view()),
+    path('project/<int:pk>/', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),
     path('pledge/<int:pk>/', views.PledgeDetail.as_view()),
 ]
